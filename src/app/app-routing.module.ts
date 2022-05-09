1bb426bb2eb5b05635f68fb05141fe2b6a4b601f
gupta-kishan <88441445+gupta-kishan@users.noreply.github.com>
2022-05-10 00:17:53 +0530

    nav-bar, routers added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { MatchPageComponent } from './components/match-page/match-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomePageComponent},
+  {path: "matches", component: MatchPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
