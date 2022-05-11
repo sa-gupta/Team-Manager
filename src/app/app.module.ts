b4a140692042cdae7cfd759ccd6bad71060fa95c
Sachin Gupta <88441445+gupta-kishan@users.noreply.github.com>
2022-05-12 01:59:42 +0530

    Registration page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,9 @@ import { MatchDetailComponent } from './components/match-detail/match-detail.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/add-match/add-match.component.ts
+++ src/app/components/add-match/add-match.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-match',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-match.component.css']
 })
 export class AddMatchComponent implements OnInit {
+
+  positions: any = ['Forward', 'Mid-Winger', 'Goal Keeper', 'Right-Winger']
+
+  registerForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    jNumber: new FormControl('')
+  });
 
   constructor() { }
 
